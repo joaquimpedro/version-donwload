ab436f0b5865f46db7c41e6d989a6f43cb37e5d8
Joaquim <pedro.quindin@gmail.com>
2013-04-08 16:56:57 -0300

    corrigindo download dos arquivos

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -1,21 +1,25 @@
 require 'pathname'
 class VersionController < ApplicationController
 
+	@@version_dir = ""
+	def initialize()
+		config = YAML.load_file("config/properties.yml")
+		@@version_dir = config['download']
+	end
+
 	def index
 		redirect_to(action: "download")
 	end
 
 	def download
-		config = YAML.load_file("config/properties.yml")
-		version_dir = config['download']
 		@file_versions = []
 
-		Dir.foreach(version_dir) do |valid_path|
+		Dir.foreach(@@version_dir) do |valid_path|
 			@file_versions << valid_path
 		end
 	end
 
 	def baixar
-		send_file(params[:path])
+		send_file(@@version_dir + File::SEPARATOR +  params[:path])
 	end
 end
